5a575445cc079a6552001f68d1693834fdc42da8
leemingue <lmg3864@naver.com>
2023-05-02 14:03:28 +0900

    23.05.02

---
+++ style scoped 활용/vue-cli/src/views/MyAbout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <br>
+    <br>
+    <h1>About</h1>
+    <br>
+    <h2>첫 번째 박스</h2>
+    <div class="global">
+      <div class="box"></div>
+    </div>
+    <br>
+    <h2>두 번째 박스</h2>
+    <div class="global">
+      <div class="box"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyAbout'
+}
+</script>
+
+<style scoped>
+  .box {
+    border: solid;
+    width: 100px;
+    height: 100px;
+    background-color: pink;
+  }
+</style>
---
+++ style scoped 활용/vue-cli/src/views/MyHome.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <br>
+    <h2>첫 번째 박스</h2>
+    <div class="global">
+      <div class="box"></div>
+    </div>
+    <br>
+    <h2>두 번째 박스</h2>
+    <div class="global">
+      <div class="box"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHome'
+}
+</script>
+
+<style>
+  .global {
+    display: flex;
+    justify-content: center;
+  }
+  .box {
+    border: solid;
+    width: 100px;
+    height: 100px;
+  }
+</style>
