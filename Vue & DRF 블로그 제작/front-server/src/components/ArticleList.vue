9026ef03edb3b02c499efe7e2611da4ec72384e2
leemingue <lmg3864@naver.com>
2023-05-16 21:11:16 +0900

    23.05.16

---
+++ Vue & DRF 블로그 제작/front-server/src/components/ArticleList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="article-list">
+    <ArticleListItem 
+    v-for="article in articles" :key="article.id" :article="article"
+    />
+  </div>
+</template>
+
+<script>
+import ArticleListItem from '@/components/ArticleListItem'
+
+export default {
+  name: 'ArticleList',
+  components: {
+    ArticleListItem,
+  },
+  computed: {
+    articles() {
+      return this.$store.state.articles
+    }
+  }
+}
+</script>
+
+<style>
+.article-list {
+  text-align: start;
+}
+</style>
---
+++ Vue & DRF 블로그 제작/front-server/src/components/ArticleListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h5>{{ article.title }}</h5>
+    <p>{{ article.content }}</p>
+    <hr>
+    <p>{{article.created_at}}</p>
+    <!-- <router-link :to="{
+      name: 'DetailView',
+      params: {id: article.id }}">
+      [DETAIL]
+    </router-link> -->
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleListItem',
+  props: {
+    article: Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
